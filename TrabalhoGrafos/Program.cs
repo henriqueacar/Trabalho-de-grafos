6f899365983ac25b09e17004079661c5d61bbe0e
Igor Westermann <i.westma@gmail.com>
2023-11-20 21:43:12 -0300

    primeira parte do trabalho

---
+++ TrabalhoGrafos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using TrabalhoGrafos.Graph;
+
+class Program
+{
+    public static void Main()
+    {
+        string filePath =
+            Path.GetFullPath("/Users/igorwestermann/Dev/UFJF/Grafos/Trabalho-de-grafos/TrabalhoGrafos/TestFile.txt");
+
+        Graph graph = new Graph(0);
+
+        graph.ReadGraph(filePath);
+        graph.DisplayInscidenceList();
+        graph.DisplayAdjacencyMatrix();
+        graph.DisplayIncidenceTable();
+
+        Console.ReadLine();
+    }
+}
