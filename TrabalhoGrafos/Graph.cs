a086e72078c502149e1443de93177c0d07905bc0
IgorWestermann <i.westma@gmail.com>
2023-12-10 20:11:47 -0300

    feature: primeira parte do trabalho

--- TrabalhoGrafos/Graph.cs
+++ TrabalhoGrafos/Graph.cs
@@ -1,6 +1,6 @@
 namespace TrabalhoGrafos.Graph;
 
-class Graph
+internal class Graph
 {
     private int[,] incidenceMatrix;
     private int[,] adjacencyMatrix;
@@ -12,63 +12,19 @@ class Graph
         adjacencyMatrix = new int[numVertices, numVertices];
         adjacencyList = new List<List<int>>(numVertices);
 
-        for (int i = 0; i < numVertices; i++)
+        for (var i = 0; i < numVertices; i++)
         {
             adjacencyList.Add(new List<int>());
         }
     }
 
-    public void ReadGraph(string filePath)
-    {
-        try
-        {
-            using (StreamReader sr = new StreamReader(filePath))
-            {
-                int numVertices = int.Parse(sr.ReadLine());
 
-                if (numVertices <= 0)
-                {
-                    Console.WriteLine("Invalid number of vertices in the file.");
-                    return;
-                }
-
-                incidenceMatrix = new int[numVertices, numVertices];
-                adjacencyMatrix = new int[numVertices, numVertices];
-                adjacencyList = new List<List<int>>();
-
-                for (int i = 0; i < numVertices; i++)
-                {
-                    string[] lineValues = sr.ReadLine().Split(' ');
-
-                    adjacencyList.Add(new List<int>());
-
-                    for (int j = 0; j < numVertices; j++)
-                    {
-                        adjacencyMatrix[i, j] = int.Parse(lineValues[j]);
-
-                        if (adjacencyMatrix[i, j] == 1)
-                        {
-                            incidenceMatrix[i, j] = 1;
-                            incidenceMatrix[j, i] = 1; // For undirected graphs
-                            adjacencyList[i].Add(j);
-                        }
-                    }
-                }
-            }
-
-            Console.WriteLine("Graph read successfully from the file.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"An error occurred: {ex.Message}");
-        }
-    }
-
+    // Displays the adjacency list representation of the graph.
     public void DisplayInscidenceList()
     {
         Console.WriteLine("Adjacency List:");
 
-        for (int i = 0; i < adjacencyList.Count; i++)
+        for (var i = 0; i < adjacencyList.Count; i++)
         {
             Console.Write($"Vertex {i}: ");
             foreach (var neighbor in adjacencyList[i])
@@ -83,13 +39,13 @@ class Graph
     public void DisplayAdjacencyMatrix()
     {
         Console.WriteLine("Incidence Matrix:");
-        
-        int rows = adjacencyMatrix.GetLength(0);
-        int cols = adjacencyMatrix.GetLength(1);
 
-        for (int i = 0; i < rows; i++)
+        var rows = adjacencyMatrix.GetLength(0);
+        var cols = adjacencyMatrix.GetLength(1);
+
+        for (var i = 0; i < rows; i++)
         {
-            for (int j = 0; j < cols; j++)
+            for (var j = 0; j < cols; j++)
             {
                 Console.Write(adjacencyMatrix[i, j] + "\t");
             }
@@ -97,25 +53,99 @@ class Graph
             Console.WriteLine();
         }
     }
+
     public void DisplayIncidenceTable()
     {
         Console.WriteLine("Incidence Table:");
 
         Console.Write("\t");
-        for (int j = 0; j < incidenceMatrix.GetLength(1); j++)
+        for (var j = 0; j < incidenceMatrix.GetLength(1); j++)
         {
             Console.Write($"({(char)('A' + j)})\t");
         }
+
         Console.WriteLine();
 
-        for (int i = 0; i < incidenceMatrix.GetLength(0); i++)
+        for (var i = 0; i < incidenceMatrix.GetLength(0); i++)
         {
             Console.Write($"{(char)('A' + i)}\t");
-            for (int j = 0; j < incidenceMatrix.GetLength(1); j++)
+            for (var j = 0; j < incidenceMatrix.GetLength(1); j++)
             {
                 Console.Write($" {incidenceMatrix[i, j]}\t");
             }
+
             Console.WriteLine();
+        }
+    }
+    
+    // Reads a graph from a file in the specified format and initializes the internal representation.
+    public void ReadGraph(string filePath)
+    {
+        try
+        {
+            using var sr = new StreamReader(filePath);
+
+            var numVertices = int.Parse(sr.ReadLine());
+
+            if (numVertices <= 0)
+            {
+                Console.WriteLine("Invalid number of vertices in the file.");
+                return;
+            }
+
+            incidenceMatrix = new int[numVertices, numVertices];
+            adjacencyMatrix = new int[numVertices, numVertices];
+            adjacencyList = new List<List<int>>();
+
+            for (var i = 0; i < numVertices; i++)
+            {
+                var lineValues = sr.ReadLine()?.Split(' ');
+
+                adjacencyList.Add(new List<int>());
+
+                for (var j = 0; j < numVertices; j++)
+                {
+                    adjacencyMatrix[i, j] = int.Parse(lineValues?[j]);
+                    incidenceMatrix[i, j] = adjacencyMatrix[i, j];
+
+                    if (adjacencyMatrix[i, j] != 0)
+                    {
+                        adjacencyList[i].Add(j);
+                    }
+                }
+            }
+
+            Console.WriteLine("Graph read successfully from the file.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+
+    public void SaveGraph(string filePath)
+    {
+        try
+        {
+            using var sw = new StreamWriter(filePath);
+
+            sw.WriteLine(adjacencyList.Count);
+
+            for (var i = 0; i < adjacencyMatrix.GetLength(0); i++)
+            {
+                for (var j = 0; j < adjacencyMatrix.GetLength(1); j++)
+                {
+                    sw.Write($"{adjacencyMatrix[i, j]} ");
+                }
+
+                sw.WriteLine();
+            }
+
+            Console.WriteLine("Graph saved successfully to the file.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while saving the graph: {ex.Message}");
         }
     }
 }
--- TrabalhoGrafos/Program.cs
+++ TrabalhoGrafos/Program.cs
@@ -5,16 +5,51 @@ class Program
 {
     public static void Main()
     {
-        string filePath =
-            Path.GetFullPath("/Users/igorwestermann/Dev/UFJF/Grafos/Trabalho-de-grafos/TrabalhoGrafos/TestFile.txt");
+        var graph = new Graph(0);
+        var graphType = String.Empty;
+        int choice;
 
-        Graph graph = new Graph(0);
+        do
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Simple Graph");
+            Console.WriteLine("2. Simple Digraph");
+            Console.WriteLine("3. Valued Simple Digraph");
+            Console.WriteLine("4. Exit");
 
-        graph.ReadGraph(filePath);
-        graph.DisplayInscidenceList();
-        graph.DisplayAdjacencyMatrix();
-        graph.DisplayIncidenceTable();
+            Console.Write("Enter your choice (1-4): ");
+            if (int.TryParse(Console.ReadLine(), out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        graphType = "GrafoSimples";
+                        break;
+                    case 2:
+                        graphType = "DigrafoSimples";
+                        break;
+                    case 3:
+                        graphType = "DigrafoSimplesValorado";
+                        
+                        break;
+                    case 4:
+                        Console.WriteLine("Exiting the program. Press Enter to close.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a number between 1 and 4.");
+                        break;
+                }
+                graph.ReadGraph($"C:/Users/iwest/RiderProjects/Trabalho-de-grafos/TrabalhoGrafos/{graphType}.txt");
+                graph.DisplayInscidenceList();
+                graph.DisplayAdjacencyMatrix();
+                graph.DisplayIncidenceTable();
+                graph.SaveGraph($"C:/Users/iwest/RiderProjects/Trabalho-de-grafos/TrabalhoGrafos/{graphType}_output.txt");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
 
-        Console.ReadLine();
+        } while (choice < 4);
     }
 }
