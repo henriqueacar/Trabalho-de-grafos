c22c86bf2db58413072d07701e30fc56289a0663
Henrique Cardoso <henrique_acar@hotmail.com>
2023-12-17 01:33:19 -0300

    Fix Prim

--- Algorithms/Prim.cs
+++ Algorithms/Prim.cs
@@ -4,9 +4,8 @@ namespace TrabalhoGrafos.Graph.Algorithms;
 
 public class Prim: IGraphAlgorithm
 {
-    public void Execute(Grafo grafo, GraphUtils graphUtils)
-    {
-        //Peso total da arvore
+        public void Execute(Grafo grafo, GraphUtils graphUtils)
+    { //Peso total da arvore
         int pesoTotal = 0;
         //Arestas que fazem parte da arvore
         var arestasArvorePrim = new List<Aresta>();
@@ -17,33 +16,42 @@ public class Prim: IGraphAlgorithm
         //Adiciona vertice atual na lista de visitados
         verticesVisitados.Add(verticeAtual);
 
+        //While para construir a arvore geradora minima
         while(verticesVisitados.Count < grafo.Vertices.Count){
+            
             //Controle para aresta de menor peso
-            Aresta menorPeso = null;
+            Aresta menorAresta = null;
+            int menorPeso = int.MaxValue;
 
-            foreach(var verticeVisitado in verticesVisitados){
+            foreach(var verticeVisitado in verticesVisitados.ToList()){
                 //Pega a lista de arestas do vertice atual da iteraçao
                 //E cria uma lista ordenada por peso
                 var arestasAdjacentes = verticeVisitado.Arestas.Where(
-                    a => !verticesVisitados.Contains(a.LeftVertice) || !verticesVisitados.Contains(a.RightVertice))
-                    .OrderBy(a => a.Peso); 
-                //Pega a menor aresta da lista
-                var arestaMenor = arestasAdjacentes.FirstOrDefault();
-                
-                //Compara a aresta da lista com a aresta menorPeso para checar
-                //qual é a menor
-                if(arestaMenor != null && (menorPeso == null || arestaMenor.Peso < menorPeso.Peso)){
-                    menorPeso = arestaMenor;
-                }
+                    a => (!verticesVisitados.Contains(a.LeftVertice) && verticesVisitados.Contains(a.RightVertice)) || 
+                    (!verticesVisitados.Contains(a.RightVertice) && verticesVisitados.Contains(a.LeftVertice)))
+                    .OrderBy( a => a.Peso);
 
-                //Adiciona a aresta da lista da arvore e soma o peso
-                if(menorPeso != null){
-                    arestasArvorePrim.Add(menorPeso);
-                    verticeAtual = verticeAtual == menorPeso.LeftVertice ? menorPeso.RightVertice
-                     : menorPeso.LeftVertice;
-                    verticesVisitados.Add(verticeAtual);
-                    pesoTotal += menorPeso.Peso;
+                //Pega aresta com o menor peso da lista
+                var arestaMenor = arestasAdjacentes.FirstOrDefault();
+
+                //Atribui a aresta de menor peso da lista na variavel de controle
+                if(arestaMenor != null && arestaMenor.Peso < menorPeso){
+                    menorAresta = arestaMenor;
+                    menorPeso = arestaMenor.Peso;
                 }
+            }
+
+            //Adiciona a aresta na arvore geradora e calcula o peso total
+            if(menorAresta != null){
+                arestasArvorePrim.Add(menorAresta);
+                pesoTotal += menorAresta.Peso;
+                //Verifica se o LeftVertice da menorAresta já foi visitado
+                //Caso sim, atribui verticeAtual com o rightVertice
+                //Caso nao, atribui verticeAtual com o leftVertice
+                verticeAtual = verticesVisitados.Contains(menorAresta.LeftVertice) 
+                ? menorAresta.RightVertice : menorAresta.LeftVertice;
+                //Adiciona o vertice na lista de verticesVisitados
+                verticesVisitados.Add(verticeAtual);
             }
         }
 
@@ -52,7 +60,7 @@ public class Prim: IGraphAlgorithm
         foreach(var aresta in arestasArvorePrim){
             Console.Write($"({aresta.LeftVertice},{aresta.RightVertice})");
         }
-        Console.WriteLine($"\nPeso:{pesoTotal}");
+        Console.WriteLine($"\nPeso:{pesoTotal}\n");
     }
     
     // Not Used
